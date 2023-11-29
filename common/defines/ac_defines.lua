dc40fd6f9ed30fcd7b1d602e2d354e692a978b43
Feveros <warfeveros@gmail.com>
2023-11-29 23:45:40 +0100

    UPDATE

--- common/defines/ac_defines.lua
+++ common/defines/ac_defines.lua
@@ -6,6 +6,7 @@ NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_COUNTRY_HIGH = 5
 
 NDefines.NMilitary.MAX_DIVISION_BRIGADE_WIDTH = 3;		-- Max width of regiments in division designer.
 NDefines.NMilitary.MAX_DIVISION_BRIGADE_HEIGHT = 5;		-- Max height of regiments in division designer.
+NDefines.NMilitary.MIN_DIVISION_BRIGADE_HEIGHT = 5;		-- Min height of regiments in division designer.
 NDefines.NMilitary.MAX_DIVISION_SUPPORT_WIDTH = 1;		-- Max width of support in division designer.
 NDefines.NMilitary.MAX_DIVISION_SUPPORT_HEIGHT = 6;		-- Max height of support in division designer.
 
